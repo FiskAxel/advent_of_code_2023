32f907d348eab7708168a136009aa0f816070e2f
Axel Sundin <awite_sundin@hotmail.com>
2023-12-14 18:56:50 +0100

    :)

--- Day05.cs
+++ Day05.cs
@@ -1,9 +1,24 @@
+using System.Collections;
+
 class Day05
 {
     public static void Solve(string[] input)
     {
         Console.WriteLine("DAY05");
 
+        List<long> seeds = input[0].Substring("seeds: ".Length)
+            .Split(' ')
+            .Where(s => long.TryParse(s, out _))
+            .Select(long.Parse)
+            .ToList();
+        int index = 3;
+        ArrayList seedToSoil = method(input, index, out index);
+        ArrayList soilToFertilizer = method(input, index, out index);
+        ArrayList fertilizerToWater = method(input, index, out index);
+        ArrayList waterToLight = method(input, index, out index);
+        ArrayList ligthToTemperature = method(input, index, out index);
+        ArrayList temperatureToHumidity = method(input, index, out index);
+        ArrayList humidityToLocation = method(input, index, out index);
         // PART 1
         int sum = 0;
         Console.WriteLine($"Part1: {sum}");
@@ -12,8 +27,20 @@ class Day05
         Console.WriteLine($"Part2: {sum}");
     }
 
-    private static int method()
+    static ArrayList method(string[] input, int index, out int i) 
     {
-        return 1;
+        i = index;
+        ArrayList map = new ArrayList();
+        while (i < input.Length && input[i] != "")
+        {
+            map.Add(input[i]
+                .Split(' ')
+                .Where(s => long.TryParse(s, out _))
+                .Select(long.Parse)
+                .ToList());
+            i++;
+        }
+        i += 2;
+        return map;
     }
 }
---
+++ Day10.cs
@@ -0,0 +1,19 @@
+class Day10
+{
+    public static void Solve(string[] input)
+    {
+        Console.WriteLine("DAY10");
+
+        // PART 1
+        int sum = 0;
+        Console.WriteLine($"Part1: {sum}");
+
+        // PART 2
+        Console.WriteLine($"Part2: {sum}");
+    }
+
+    private static int method()
+    {
+        return 1;
+    }
+}
