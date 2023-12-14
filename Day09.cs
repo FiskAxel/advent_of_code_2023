9d685b5853c34c487f9e80769a7c4c6cff93c442
Axel Sundin <awite_sundin@hotmail.com>
2023-12-14 18:52:00 +0100

    Day09

--- Day09.cs
+++ Day09.cs
@@ -1,19 +1,58 @@
+using System.Security.Cryptography;
+
 class Day09
 {
     public static void Solve(string[] input)
     {
         Console.WriteLine("DAY09");
 
-        // PART 1
-        int sum = 0;
-        Console.WriteLine($"Part1: {sum}");
+        int sum1 = 0;
+        int sum2 = 0;
+        foreach (string line in input)
+        {
+            List<List<int>> history = new List<List<int>>();
+            history.Add(line.Split(' ')
+                            .Select(int.Parse)
+                            .ToList());
+            while (history[history.Count - 1].Sum() != 0)
+            {
+                history.Add(NextHistory(history[history.Count - 1]));
+            }
 
-        // PART 2
-        Console.WriteLine($"Part2: {sum}");
+            sum1 += CalculateNextSequence(history);
+            sum2 += CalculatePreviousSequence(history);
+        }
+        Console.WriteLine($"Part1: {sum1}");
+        Console.WriteLine($"Part2: {sum2}");
     }
 
-    private static int method()
+    static List<int> NextHistory(List<int> prevHistory)
     {
-        return 1;
+        List<int> nextHistory = new List<int>();
+        for (int i = 0; i < prevHistory.Count - 1; i++)
+        {
+            nextHistory.Add(prevHistory[i + 1] - prevHistory[i]);
+        }
+        return nextHistory;
+    }
+
+    static int CalculateNextSequence(List<List<int>> history)
+    {
+        for (int i = history.Count - 1;  --i >= 0;)
+        {
+            int nextSequence = history[i].Last() + history[i + 1].Last();
+            history[i].Add(nextSequence);
+        }
+        return history[0].Last();
+    }
+
+    static int CalculatePreviousSequence(List<List<int>> history)
+    {
+        for (int i = history.Count - 1; --i >= 0;)
+        {
+            int previousSequence = history[i][0] - history[i + 1][0];
+            history[i].Insert(0, previousSequence);
+        }
+        return history[0][0];
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@
         Day06.Solve(GetInput("input06.txt")); // 227850     42948149
         //Day07.Solve(GetInput("input07.txt")); // 
         //Day08.Solve(GetInput("input08.txt")); // 
-        //Day09.Solve(GetInput("input09.txt")); // 
+        Day09.Solve(GetInput("input09.txt")); // 1868368343 1022
         //Day10.Solve(GetInput("input10.txt")); // 
         //Day11.Solve(GetInput("input11.txt")); // 
         //Day12.Solve(GetInput("input12.txt")); // 
