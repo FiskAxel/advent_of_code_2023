584575792e724f41b972ad63ee71f3fbcf4f7bd1
Axel Sundin <awite_sundin@hotmail.com>
2023-12-14 22:35:34 +0100

    Day08

--- Day08.cs
+++ Day08.cs
@@ -1,19 +1,101 @@
+using System.Collections;
+
 class Day08
 {
     public static void Solve(string[] input)
     {
         Console.WriteLine("DAY08");
 
+        string instructions = input[0];
+        Dictionary<string, Node> nodes = new Dictionary<string, Node>();
+        List<string> positions = new List<string>();
+        for (int i = 2; i < input.Count(); i++)
+        {
+            string k = input[i].Substring(0, 3);
+            string L = input[i].Substring(7, 3);
+            string R = input[i].Substring(12, 3);
+            nodes[k] = new Node(L, R);
+            if (k.Last() == 'A') positions.Add(k);
+        }
+
         // PART 1
-        int sum = 0;
-        Console.WriteLine($"Part1: {sum}");
+        long steps = 0;
+        int ins = 0;
+        string current = "AAA";
+        while (current != "ZZZ")
+        {
+            steps++;
+            current = instructions[ins] switch
+            {
+                'L' => nodes[current].L,
+                'R' => nodes[current].R,
+                _ => "ERROR"
+            };
+            ins = (ins + 1) % instructions.Length;
+        }
+        Console.WriteLine($"Part1: {steps}");
 
         // PART 2
-        Console.WriteLine($"Part2: {sum}");
+        steps = 0;
+        Stack<long> l = new Stack<long>();
+        foreach (string position in positions)
+        {
+            l.Push(Steps(nodes, instructions, position));
+        }
+        Console.WriteLine($"Part2: {LCM(l)}");
     }
 
-    private static int method()
+    static int Steps(Dictionary<string, Node> nodes, string instructions, string current)
     {
-        return 1;
+        int steps = 0;
+        int ins = 0;
+        while (current.Last() != 'Z')
+        {
+            steps++;
+            current = instructions[ins] switch
+            {
+                'L' => nodes[current].L,
+                'R' => nodes[current].R,
+                _ => "ERROR"
+            };
+            ins = (ins + 1) % instructions.Length;
+        }
+        return steps;
+    }
+
+    static long LCM(Stack<long> l)
+    {
+        while (l.Count > 1)
+        {
+            l.Push(lcm(l.Pop(), l.Pop()));
+        }
+        return l.Pop();
+    }
+
+    static long lcm(long a, long b)
+    {
+        return (a / gcf(a, b)) * b;
+    }
+    
+    static long gcf(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public class Node
+    {
+        public string L;
+        public string R;
+        public Node(string L, string R)
+        {
+            this.L = L;
+            this.R = R;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
         //Day05.Solve(GetInput("input05.txt")); // 
         Day06.Solve(GetInput("input06.txt")); // 227850     42948149
         //Day07.Solve(GetInput("input07.txt")); // 
-        //Day08.Solve(GetInput("input08.txt")); // 
+        Day08.Solve(GetInput("input08.txt")); // 20093      22103062509257
         Day09.Solve(GetInput("input09.txt")); // 1868368343 1022
         //Day10.Solve(GetInput("input10.txt")); // 
         //Day11.Solve(GetInput("input11.txt")); // 
